7922f7b78b4bea187bcfb100a5e7a7717e79c01b
Artyom Guryanov <bishop81285@gmail.com>
2023-07-20 22:59:33 +0600

    linked_list.py realisation inserting

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,29 +1,67 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+
+    def __init__(self, data, next_node=None):
+        """
+        Конструктор класса Node
+
+        :param data: данные, которые будут храниться в узле
+        """
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
+    def __init__(self):
+        """Конструктор класса LinkedList"""
+        self.head = None
+        self.tail = None
+
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        new_node = Node(data)
+
+        if self.is_empty():
+            self.head = self.tail = new_node
+            self.tail.next_node = None
+        else:
+            new_node.next_node = self.head
+            self.head = new_node
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        new_node = Node(data)
+
+        if self.is_empty():
+            self.head = self.tail = new_node
+            self.tail.next_node = None
+        else:
+            new_node.next_node = None
+            self.tail.next_node = new_node
+            self.tail = new_node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
         node = self.head
+
         if node is None:
             return str(None)
 
         ll_string = ''
+
         while node:
             ll_string += f' {str(node.data)} ->'
             node = node.next_node
 
         ll_string += ' None'
         return ll_string
+
+    def is_empty(self):
+        """
+        Метод проверяющий пустоту списка
+
+        :return: True если список пуст, False в противном случае
+        """
+        return self.head is None
