6d7058e8d36649a8ddeea0507360914265b8d87b
Artyom Guryanov <bishop81285@gmail.com>
2023-07-27 21:59:19 +0600

    add tests for new methods in linked_list.py

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,6 +1,9 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
 import unittest
 
+import sys
+from io import StringIO
+
 from src.linked_list import LinkedList
 
 
@@ -37,3 +40,24 @@ class TestLinkedList(unittest.TestCase):
 
         ll.insert_beginning(data)
         self.assertFalse(ll.is_empty())
+
+    def test_to_list(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        ll.insert_at_end('idusername')
+        ll.insert_at_end([1, 2, 3])
+        ll.insert_at_end({'id': 2, 'username': 'mosh_s'})
+
+        self.assertEqual(ll.to_list(), [{'id': 1, 'username': 'lazzy508509'}, 'idusername', [1, 2, 3],
+                                             {'id': 2, 'username': 'mosh_s'}])
+
+    def test_get_data_by_id(self):
+        ll = LinkedList()
+        ll.insert_beginning({'id': 1, 'username': 'lazzy508509'})
+        ll.insert_at_end('idusername')
+        ll.insert_at_end([1, 2, 3])
+        ll.insert_at_end({'id': 2, 'username': 'mosh_s'})
+
+        self.assertEqual(ll.get_data_by_id(1), {'id': 1, 'username': 'lazzy508509'})
+        self.assertEqual(ll.get_data_by_id(2), {'id': 2, 'username': 'mosh_s'})
+        self.assertEqual(ll.get_data_by_id(3), None)
